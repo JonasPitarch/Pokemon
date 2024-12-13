5c8b5a949ed33be60c28c18df196c3a861ceb716
jonpitsaf <jonaspitarch06@gmail.com>
2024-12-13 12:34:55 +0100

    Incidencias

--- Incidencias/app/src/main/java/com/example/incidencias/ui/home/HomeFragment.java
+++ Incidencias/app/src/main/java/com/example/incidencias/ui/home/HomeFragment.java
@@ -1,7 +1,17 @@
 package com.example.incidencias.ui.home;
+import static android.content.ContentValues.TAG;
+
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.health.connect.datatypes.ExerciseRoute;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +25,21 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.incidencias.databinding.FragmentHomeBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private ActivityResultLauncher<String[]> locationPermissionRequest;
+    private Location mLastlocation;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Context mContext;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
@@ -45,6 +65,7 @@ public class HomeFragment extends Fragment {
                     }
                 }
         );
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
 
         return root;
 
@@ -62,6 +83,62 @@ public class HomeFragment extends Fragment {
             });
         } else {
             Toast.makeText(requireContext(), "getLocation: permissions granted", Toast.LENGTH_SHORT).show();
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(
+                    location -> {
+                        if (location != null) {
+                            mLastlocation = location;
+                            binding.localitzacio.setText(
+                                    String.format("Latitud: %1$.4f \n Longitud: %2$.4f\n Hora: %3$tr",
+                                            mLastlocation.getLatitude(),
+                                            mLastlocation.getLongitude(),
+                                            mLastlocation.getTime()));
+                        }
+                        else {
+                            binding.localitzacio.setText("Sin locaclización conocida");
+                        }
+                    }
+            );
+        }
+    }
+
+    private void fetchAddress(){
+        Geocoder geocoder = new Geocoder(mContext,
+                Locale.getDefault());
+        List<Address>addresses = null;
+        String resultMessage="";
+
+        try {
+            addresses = geocoder.getFromLocation(
+                    location.getLatitude(),
+                    location.getLongitude()
+            );
+            if (addresses == null || addresses.size() == 0) {
+                if (resultMessage.isEmpty()) {
+                    resultMessage = "No s'ha trobat cap adreça";
+                    Log.e(TAG, resultMessage);
+                }
+            }
+            else {
+                Address address = addresses.get(0);
+                ArrayList<String>addressParts=new ArrayList<>();
+
+                for (int i =0 ; i<=address.getMaxAddressLineIndex(); i++){
+                    addressParts.add(address.getAddressLine(i));
+                }
+
+                resultMessage = TextUtils.join("\n", addressParts);
+            }
+        }
+        catch (IOException e){
+            resultMessage = "Servei no disponible";
+            Log.e(TAG,resultMessage,e);
+        }
+        catch (IllegalArgumentException illegalArgumentException){
+            resultMessage="Coordenades no valides";
+            Log.e(TAG, resultMessage + ". " +
+                    "Latitude = " + location.getLatitude() +
+                    ", Longitude = " +
+                    location.getLongitude(), illegalArgumentException);
         }
     }
 
